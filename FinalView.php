c78da659f271950ebb9d08569a4c3fccab22342a
Hans Gundlach <hans.gundlach@gmail.com>
2016-11-24 16:25:22 -0800

    Updated Positioning

--- FinalView.php
+++ FinalView.php
@@ -1,12 +1,4 @@
 <html>
-
-
-<?php
-// Start my session at the very beginning of the page
-// I only need to do this because I may be using the $_SESSION variable
-session_start();
-?>
-
 <head>
 <link rel= "stylesheet" type = "text/css" href= "Final.css">
 <!-- latest version of jquery -->
@@ -19,14 +11,51 @@ session_start();
 </head>
 
 
+<?php
+// Start my session at the very beginning of the page
+// I only need to do this because I may be using the $_SESSION variable
+session_start();
+?>
 
 
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+	if (isset($_POST['name']))
+	{
+		// Now the session variable "name" is set.
+		// This means that any other PHP pages on this site
+		// will have access to this variable until the user
+		// closes the browser
+		$_SESSION['examplePage_name'] = $_POST['name'];
+    //I'm setting another php variable
 
-<div class = "text-center">
+
+	}
+?>
+
+
+  </p>
+<?php
+}
+else
+{
+?>
+
+<?php
+}
+?>
+
+
 <div class = "jumbotron">
+<p id="intro"> Hello, <?= $_SESSION['examplePage_name'] ?></p>
+<p id = "score"> Your score is <?= $_SESSION['score'] ?> </p>
+<div class="text-center">
 <h1 class ="Main24">24</h1>
 </div>
+</div>
+<div class = "text-center">
 <p>
   <h2>
    Your Four Numbers Are : <span id= "Numbers"> </span>
@@ -72,63 +101,6 @@ For Example : 2*6 + 2*6 Works
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-	if (isset($_POST['name']))
-	{
-		// Now the session variable "name" is set.
-		// This means that any other PHP pages on this site
-		// will have access to this variable until the user
-		// closes the browser
-		$_SESSION['examplePage_name'] = $_POST['name'];
-    //I'm setting another php variable
-
-
-	}
-?>
-	<p id="intro"> Hello, <?= $_SESSION['examplePage_name'] ?>! Your name has been saved to a session variable, which will be convenient later. Now, Let's have some fun with AJAX. I'm going to generate the text below by accessing data from another PHP page entirely, but without reloading the page! Go ahead, click a button.</p>
-	<!-- Below, we use the onclick event to run a Javascript function (defined above) when you click on a button! This is similar to AppLab's onEvent functionality.. -->
-    You score
-  <p id = "score"> Your score is <?= $_SESSION['score'] ?> </p>
-
-  </p>
-<?php
-}
-else
-{
-?>
-
   <p id="intro"> Hello, <?= $_SESSION['examplePage_name'] ?></p>
   <!-- Below, we use the onclick event to run a Javascript function (defined above) when you click on a button! This is similar to AppLab's onEvent functionality.. -->
     You score
@@ -136,12 +108,6 @@ else
 
 
 
-
-
-
-<?php
-}
-?>
 <h2>
 <a href= "HighScorePage.php">High Score Page</a>
 </h2>
